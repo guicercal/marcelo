f02ba83509cb0452a589743fa8b0735b3a76161a
Guilherme Cercal <gui@colab.coop>
2024-04-15 19:07:01 -0300

    nav & hero - initial
    Navegacao + inicio da secao hero

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# marcelo
+# Website Marcelo
+
+Você deverá ter instalado o Node.js e o NPM para poder trabalhar neste projeto, bem como um SCSS watcher para precompilar
+
+- no seu terminal, navegue até a pasta do projeto `cd /caminho/da/pasta/do/projeto` e entao execute o codigo `npm install`  para instalar todas as dependencias do projeto;
+- Seu watcher de SCSS deverá estar ativo e escutando as modificacoes no arquivo **style.css**
---
+++ assets/js/scripts.js
@@ -0,0 +1,39 @@
+$(function (){
+
+  const mainHeader = $('#main-header');
+  // Função de throttle para limitar a frequência de execução
+  function throttle(func, limit) {
+    var inThrottle;
+    return function() {
+      var args = arguments,
+        context = this;
+      if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        setTimeout(function() {
+          inThrottle = false;
+        }, limit);
+      }
+    };
+  }
+
+  // Função para verificar a posição de rolagem e aplicar a classe
+  function checkScroll() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 219) {
+      mainHeader.addClass("sticky");
+    } else {
+      mainHeader.removeClass("sticky");
+    }
+  }
+
+  // Executar a função de verificação de rolagem usando throttle
+  $(window).scroll(throttle(checkScroll, 10));
+
+  $('#mobile-menu-opener, .main-header__nav li a').click(function () {
+    mainHeader.toggleClass('opened');
+  });
+
+  checkScroll();
+
+});
